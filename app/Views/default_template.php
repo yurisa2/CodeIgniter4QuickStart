4c9ab44267f1c4018540fd974def72c90d8ff0c0
Matheus <matheussouzaferro@gmail.com>
2020-06-28 21:50:52 -0300

    Adicionado os cripts necessarios

--- app/Views/default_template.php
+++ app/Views/default_template.php
@@ -169,7 +169,9 @@
 		<script src="https://code.jquery.com/jquery-2.2.4.min.js"
 			  integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44="
 			  crossorigin="anonymous"></script>
-		
+			<!-- scripts adicionados -->
+			<script src="<?php echo base_url("public/js/bootstrap.min.js")?>"></script>
+			<script src="<?php echo base_url("public/js/ace-elements.min.js")?>"></script>
 		<script src="<?php echo base_url("public/js/ace.min.js")?>"></script>
 	</body>
 </html>
